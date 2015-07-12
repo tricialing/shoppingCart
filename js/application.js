d41b4e6dc3b584f5e055edf378145cfc6cfb23f6
Tricia Ling <tricia@mydiylab.com>
2015-07-12 15:13:07 +0800

    Fix: return subtotal. Add: new item & prepend

--- js/application.js
+++ js/application.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     var itemLength = itemPrices.length;
 
     var totalPrice = 0;
-    var subTotalItemPrices;
+    var subTotalItemPrice;
 
     for (var i = 0; i < itemLength; i++) {
       // select item price
@@ -20,16 +20,15 @@ $(document).ready(function(){
       // console.log("QTY: ", itemQuantity);   
       
       //item sub-total
-      var subTotalItemPrices = itemPrice * itemQuantity;
-      // console.log("Item subtotal : ", subTotalItemPrices);
+      var subTotalItemPrice = itemPrice * itemQuantity;
+      // console.log("Item subtotal : ", subTotalItemPrice);
 
       //update sub-total
       var itemSubTotalElement = $('.item-sub-total')[i];
       var itemSubTotalObject = $(itemSubTotalElement);
-      $('.item-sub-total p').html().replace('$'+ subTotalItemPrices.toFixed(2));
-      // console.log("Item SubTotal: ", subTotalItemPrices);
+      $(itemSubTotalObject).text('$' + subTotalItemPrice);
 
-      totalPrice += subTotalItemPrices;
+      totalPrice += subTotalItemPrice;
     }
       //Update total-price 
       console.log("Total price : ", totalPrice);    
@@ -42,9 +41,38 @@ $(document).ready(function(){
     calcTotalPrice();
   });
 
-    $('.cancel').click(function() {
+  $('.cancel').click(function() {
     $(this).parent().parent().remove();
   });
+
+  $('.add-item').click(function() {
+    var itemName= $('#new-item-name').val();
+    var itemUnitPrice = $('#new-item-price').val();
+    var newItem = '<div class="item row col-xs-12"> <p class="item-name col-sm-3">' + itemName + '</p><p class="item-price col-sm-3">' + itemUnitPrice + '</p> <form class="item-qty col-sm-4 form-inline"><div class="input-group"> <label class="input-group-addon">QTY</label> <input class="quantity form-control" type="text"> </div> <button class="cancel btn btn-default">cancel</button> </form> <p class="col-sm-2 item-sub-total">$ - </p> </div>'
+    // html += '<div class="item row col-xs-12">'
+    // html += ' <p class="item-name col-sm-3">'
+    // html +=   itemName
+    // html += ' </p>'
+    // html += ' <p class="item-price col-sm-3">'
+    // html +=     itemUnitPrice
+    // html += ' </p>'
+    // html += ' <form class="item-qty col-sm-4 form-inline">'
+    // html += '   <div class="input-group">'
+    // html += '     <label class="input-group-addon">QTY</label>'
+    // html += '     <input class="quantity form-control" type="text"> '
+    // html += '   </div>'
+    // html += '   <button class="cancel btn btn-default">cancel</button>' 
+    // html += ' </form>'
+    // html += ' <p class="col-sm-2 item-sub-total">'
+    // html += '   $ -'
+    // html += ' </p>'
+    // html += '</div>'
+    // var newItem = html;
+    $('#shoppingList').prepend(newItem);
+    console.log("clicked");
+  });
+
+
 
 
   // $('.cancel').click(function(){
